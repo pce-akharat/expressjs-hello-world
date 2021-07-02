5ce89e2f0126c2c52391880d844ff6a922120374
Amol Kharat <akharat@mes.ac.in>
2021-07-02 15:30:06 +0530

    index file changed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
-app.listen(port);
+app.listen(port, host, function() {
+    console.log("Server started.......");
+  });
